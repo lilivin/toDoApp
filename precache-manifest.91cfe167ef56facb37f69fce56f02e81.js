e34b669ab7e36fd37b3a505bb97d5c2fb5f0af37
Karol <56819265+lilivin@users.noreply.github.com>
2020-03-05 12:46:07 +0100

    Updates

---
+++ precache-manifest.91cfe167ef56facb37f69fce56f02e81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d86cebe311abab343296",
+    "url": "/toDoApp/static/js/runtime~main.d86cebe3.js"
+  },
+  {
+    "revision": "5879270c9a6d9a5bd67d",
+    "url": "/toDoApp/static/js/main.5879270c.chunk.js"
+  },
+  {
+    "revision": "0e5f4e5ceb11873a998d",
+    "url": "/toDoApp/static/js/1.0e5f4e5c.chunk.js"
+  },
+  {
+    "revision": "5879270c9a6d9a5bd67d",
+    "url": "/toDoApp/static/css/main.d92afd49.chunk.css"
+  },
+  {
+    "revision": "d441e41cca52bcceb2c7b3e6e56f23b8",
+    "url": "/toDoApp/index.html"
+  }
+];
